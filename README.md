41771df691ef2ba2e6be59240c68f4877aaf0e42
Oak Chantosa <oak.chantosa@hingehealth.com>
2022-01-27 10:49:49 -0800

    Configure tailwind css

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# meteor-wallet
+Meteor Wallet
+- Create a new collection for Contacts
+- Insert and fetch Contacts
+- Deploy to Galaxy
+- Styled with Tailwind CSS
--- server/main.js
+++ server/main.js
@@ -1,6 +1,5 @@
 import { Meteor } from 'meteor/meteor';
-import '../imports/api/ContactsCollection.js';
+import "../imports/api/ContactsCollection";
 
 Meteor.startup(() => {
- 
 });
